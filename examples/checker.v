3531c905fb5a9df438749174fa4a53f332df086b
Dylan Donnell <dylan.donnell@student.griffith.ie>
2024-12-17 21:43:42 +0000

    feat(examples): add checker example

---
+++ examples/checker.v
@@ -0,0 +1,16 @@
+import vbmp
+
+const width = 1920
+const height = 1080
+
+fn main() {
+	mut bitmap := vbmp.new(width, height)
+
+	for x in 0 .. width {
+		for y in 0 .. height {
+			bitmap.set_pixel(x, y, (x - y) % 255, (x + y) % 255, (x * y) % 255)!
+		}
+	}
+
+  bitmap.write('checker.bmp')!
+}
